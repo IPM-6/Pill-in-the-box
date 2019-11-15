523d77e0c20f80c827461f5e60f184580ca01506
IPM-6 <55982091+IPM-6@users.noreply.github.com>
2019-11-15 11:39:11 +0000

    characters rest stock

--- prototype/js/rest.js
+++ prototype/js/rest.js
@@ -1,7 +1,222 @@
+function findAll() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.remove("hide")
+  }
+}
+
 function findA() {
   var x = document.getElementsByClassName("letter");
   for(var i = 0; i < x.length; i++){
     x[i].classList.add("hide")
   }
   document.getElementById("A").classList.remove("hide");
+}
+
+function findB() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("B").classList.remove("hide");
+}
+
+function findC() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("C").classList.remove("hide");
+}
+
+function findD() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("D").classList.remove("hide");
+}
+
+function findE() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("E").classList.remove("hide");
+}
+
+function findF() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("F").classList.remove("hide");
+}
+
+function findG() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("G").classList.remove("hide");
+}
+
+function findH() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("H").classList.remove("hide");
+}
+
+function findI() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("I").classList.remove("hide");
+}
+
+function findJ() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("J").classList.remove("hide");
+}
+
+function findK() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("K").classList.remove("hide");
+}
+
+function findL() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("L").classList.remove("hide");
+}
+
+function findM() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("M").classList.remove("hide");
+}
+
+function findN() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("N").classList.remove("hide");
+}
+
+function findO() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("O").classList.remove("hide");
+}
+
+function findP() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("P").classList.remove("hide");
+}
+
+function findQ() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("Q").classList.remove("hide");
+}
+
+function findR() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("R").classList.remove("hide");
+}
+
+function findS() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("S").classList.remove("hide");
+}
+
+function findT() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("T").classList.remove("hide");
+}
+
+function findU() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("U").classList.remove("hide");
+}
+
+function findV() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("V").classList.remove("hide");
+}
+
+function findW() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("W").classList.remove("hide");
+}
+
+function findX() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("X").classList.remove("hide");
+}
+
+function findY() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("Y").classList.remove("hide");
+}
+
+function findZ() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("Z").classList.remove("hide");
+}
+
+function findOther() {
+  var x = document.getElementsByClassName("letter");
+  for(var i = 0; i < x.length; i++){
+    x[i].classList.add("hide")
+  }
+  document.getElementById("Other").classList.remove("hide");
 }
