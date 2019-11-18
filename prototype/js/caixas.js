b40b97395726448659cf2f981a225f4d59b276e7
IPM-6 <55982091+IPM-6@users.noreply.github.com>
2019-11-18 12:42:28 +0000

    Stock e Caixas

---
+++ prototype/js/caixas.js
@@ -0,0 +1,13 @@
+function selectAll(){
+  var x = document.getElementsByClassName("person");
+  for(var i = 0; i < x.length; i++){
+    x[i].checked = "checked";
+  }
+}
+
+function deselectAll(){
+  var x = document.getElementsByClassName("person");
+  for(var i = 0; i < x.length; i++){
+    x[i].checked = "";
+  }
+}
