c15903589ac12a08fa3786f9bde017ed71bcf1eb
IPM-6 <55982091+IPM-6@users.noreply.github.com>
2019-11-21 15:03:51 +0000

    Access data getData()

--- prototype/js/stock.js
+++ prototype/js/stock.js
@@ -1,55 +1,20 @@
-var getXMLFile = function(path, callback){
-  var request = new XMLHttpRequest();
-  request.open("GET", path);
-  request.setRequestHeader("Content-Type", "text/xml");
-  request.onreadystatechange = function(){
-    if(request.readyState === 4 && request.status === 200){
-      callback(request.responseXML);
-    }
-  };
-  request.send();
-};
-
 function getData(){
-  return getXMLFile("data.xml", function(xml){
-    console.log(xml);
-    return xml;
-  });
-}
+  var xmlhttp = {};
+  var xmlDoc = {};
 
+  if (window.XMLHttpRequest) {
+    xmlhttp = new XMLHttpRequest();
+  }
+  xmlhttp.open("GET", "data.xml", false);
+  xmlhttp.send();
+  xmlDoc = xmlhttp.responseXML;
 
-
-function showMedicine(){
-
-var xmlhttp = {};
-        var xmlDoc = {};
-
-        if (window.XMLHttpRequest) {
-            xmlhttp = new XMLHttpRequest();
-        }
-        xmlhttp.open("GET", "data.xml", false);
-        xmlhttp.send();
-        xmlDoc = xmlhttp.responseXML;
-
-        console.log(xmlDoc);
-  
-  //var xml = '<?xml version="1.0" encoding="UTF-8"?><root><pill><name>Atarax</name><solves>Alergias</solves><num>10</num></pill><pill><name>Ibuprofen</name><solves>Dores</solves><num>5</num></pill><pill><name>Kompensan</name><solves>Azia</solves><num>20</num></pill><pill><name>Nasomet</name><solves>Alergias</solves><num>3</num></pill><pill><name>Omeprazol</name><solves>Dores</solves><num>50</num></pill><pill><name>Telfast</name><solves>Alergias</solves><num>39</num></pill><pill><name>Xanax</name><solves>Ansiedade</solves><num>2</num></pill><pill><name>Zyrtec</name><solves>Alergias</solves><num>23</num></pill></root>';
-  //console.log(xml);
-  //var parser = new DOMParser();
-  //var xmlDoc = parser.parseFromString(xml, "text/xml");
-  //console.log(xmlDoc);
-  
-  
+  console.log(xmlDoc);
   
   var names = xmlDoc.getElementsByTagName("name");
   var solves = xmlDoc.getElementsByTagName("solves");
   var num = xmlDoc.getElementsByTagName("num");
-  //console.log(names);
-  
-  var endButton = '<div id="addToStockButton" class="col s12 center-align"><br>';
-  endButton += '<a class="btn-floating btn-medium waves-effect waves-light red modal-trigger" href="#stockForm1">';
-  endButton += '<i class="material-icons">add</i></a></div>';
-  
+
   var comprimidos = [];
   for(var i = 0; i < names.length; i++){
     //console.log("In for: " + names[i].childNodes[0].nodeValue);
@@ -62,6 +27,18 @@ var xmlhttp = {};
     comprimidos.push(newPill);
     //console.log(comprimidos);
   }
+  return comprimidos;
+}
+
+
+
+function showMedicine(){
+
+  var comprimidos = getData();
+  
+  var endButton = '<div id="addToStockButton" class="col s12 center-align"><br>';
+  endButton += '<a class="btn-floating btn-medium waves-effect waves-light red modal-trigger" href="#stockForm1">';
+  endButton += '<i class="material-icons">add</i></a></div>';
   
   var content = "";
   for(var i = 0; i < comprimidos.length; i++){
